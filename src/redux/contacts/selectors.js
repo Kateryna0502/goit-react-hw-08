682233036be03e8796cc01ce64c9ca6d0bf22aaf
Kateryna <mweoliss74@gmail.com>
2024-09-21 21:03:33 +0300

    u

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -9,7 +9,7 @@ import { selectNameFilter } from "../filters/selectors"
 
 
 
-export const selectContacts = (state) => state.contacts.items;
+export const selectContacts = (state) => state.contacts.contacts;
 export const selectLoading = (state) => state.contacts.loading;
 export const selectError = (state) => state.contacts.error;
 
--- src/redux/filters/selectors.js
+++ src/redux/filters/selectors.js
@@ -1,1 +1,4 @@
-export const selectNameFilter = (state) => state.filters.filterValue;
+// export const selectNameFilter = (state) => state.filters.filterValue;
+
+export const selectContacts = (state) => state.contacts.contacts;
+export const selectNameFilter = (state) => state.filter.filterValue;
